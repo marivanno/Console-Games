24d431b68798f78093c0a2f46a4a4ed31acb48a8
Ivan Shadrin <chaubakaivan@gmail.com>
2020-07-17 16:53:55 +0300

    index.js comment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import readlineSync from 'readline-sync';
 
 const startGame = (fun) => {
+// Displaying a greeting and initializing variables for the loop.
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   let i = 0;
   let j = true;
   while (i < 3 && j === true) {
+  // Accept incoming data.
     const [result, description, question] = fun();
+    // Displaying desqription and question.
     console.log(description);
     console.log(question);
+    // User to enter a response.
     const answer = readlineSync.question('Enter you answer please: ');
+    // Cheking for valid result
     if (result === answer) {
       console.log('Correct!');
       i += 1;
