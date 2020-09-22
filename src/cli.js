6849b996f93c836e39a69bb228601cb6a0cfba91
Ivan Shadrin <chaubakaivan@gmail.com>
2020-09-22 18:29:41 +0300

    added ';'

--- src/cli.js
+++ src/cli.js
@@ -3,5 +3,5 @@ import readlineSync from 'readline-sync';
 const helloName = () => {
   const name = readlineSync.question('What is you name? \n');
   console.log(`Hello, ${name}!`);
-}
+};
 export default helloName;
