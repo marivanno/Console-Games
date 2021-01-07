f8849a42d79358ab8f86e8f90010ddb49ee95a57
Ivan Shadrin <chubakaivan@gmail.com>
2021-01-07 13:41:31 +0300

    test 2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const startGame = (roundData) => {
     if (result === answer) {
       console.log('Correct!');
     } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${result}.\n Let's try again, ${name}`);
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${result}.\n Let's try again, ${name}!`);
       break;
     }
     if (countOfRound === 2) {
